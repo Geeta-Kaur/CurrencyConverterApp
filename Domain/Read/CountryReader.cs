e111d94a2f633efdd6b8db2d4b8c8b1de1ce568c
Geeta-Kaur <khalsageeta@gmail.com>
2019-12-05 00:07:44 +0000

    Update CountryReader.cs

--- Domain/Read/CountryReader.cs
+++ Domain/Read/CountryReader.cs
@@ -9,39 +9,29 @@ namespace ConverterAPI.Domain.Read
     public class CountryReader : ICountryReader
     {
         ///Gets the list of country        
-          public  async Task<List<Country>> GetCountriesAsync(){
-            return await Task.Run(() => new List<Country>() {
+          public async Task<List<Country>> GetCountriesAsync(){
+            return await Task.Run(() => GetCountryList());
+    }
+       
+     private List<Country> GetCountryList(){
+            return new List<Country>() {
                 new Country {
                     Name = "United Kingdom",
-                    CurrencyName = Enums.CurrencyType.GBP,
-                    ExchangeRate = 80m
+                    CurrencyName = Enums.CurrencyType.GBP                    
                 },
                 new Country {
                     Name = "USA",
-                    CurrencyName = Enums.CurrencyType.USD,
-                    ExchangeRate = 60.05m
+                    CurrencyName = Enums.CurrencyType.USD
                 },
                 new Country {
                     Name = "Australia",
-                    CurrencyName = Enums.CurrencyType.AUD,
-                    ExchangeRate = 50.70m
+                    CurrencyName = Enums.CurrencyType.AUD
                 },
                 new Country {
                     Name = "EUROPE",
-                    CurrencyName = Enums.CurrencyType.EUR,
-                    ExchangeRate = 16.0m
+                    CurrencyName = Enums.CurrencyType.EUR
                 }
-            });
-            
-        }
-
-        public async Task<Country> GetCountryByCurrencyTypeAsync(CurrencyType currencyType)
-        {
-            return await Task.Run(()=> new Country {
-                    Name = "United Kingdom",
-                    CurrencyName = Enums.CurrencyType.GBP,
-                    ExchangeRate = 80m
-                });
+            };
         }
     }
 }
