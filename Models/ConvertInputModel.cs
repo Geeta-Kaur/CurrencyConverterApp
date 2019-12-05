aabd51ae5353f51830765045ce18e35c03e3d5ec
Geeta-Kaur <khalsageeta@gmail.com>
2019-12-05 00:10:01 +0000

    Update ConvertInputModel.cs

--- Models/ConvertInputModel.cs
+++ Models/ConvertInputModel.cs
@@ -4,10 +4,8 @@ namespace ConverterAPI.Models
 {
     public class ConvertInputModel{
         public CurrencyType ConvertFrom {get;set;}
-        public CurrencyType ConvertTo {get;set;}
-        public decimal ValueTo {get;set;}
-        public decimal ValueFrom {get;set;}
-
+        public CurrencyType ConvertTo {get;set;}        
+        public decimal ValueToConvert {get;set;}
 
     }
 }
