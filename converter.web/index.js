65503c08ec0d4ec6b2bddaca9e11d86ccdd24ca2
Geeta-Kaur <khalsageeta@gmail.com>
2019-12-10 01:29:50 +0000

    Delete index.js

--- converter.web/index.js
+++
@@ -1,6 +0,0 @@
-$.getJSON( "/api/converter/allcurrencies", function( data ) {
-  var options = $("#curencylist");
-  $.each( data, function(key, val) {
-    options.append(new Option(key, val));
-  });
-});
